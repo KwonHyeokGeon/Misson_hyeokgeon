728d5119c668993e57a5cfd7fbcc79ab53c94d65
KwonHyeokGeon <weekend12@icloud.com>
2024-02-04 22:50:51 +0900

    정적리소스 매핑 설정

---
+++ src/main/java/com/example/mission/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.example.mission;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/node_modules/**")
+                .addResourceLocations("classpath:/static/node_modules/");
+    }
+}
